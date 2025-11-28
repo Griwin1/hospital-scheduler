80f827fb5ef262f38895cc832b8b2b43a127d6b1
Griwin1 <nikolaorlovski37@gmail.com>
2025-11-29 00:11:36 +0200

    Small workflow changes

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hospital scheduler header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Hospital Scheduler/i);
-  expect(headerElement).toBeInTheDocument();
-});
